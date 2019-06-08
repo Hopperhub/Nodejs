2492b4684cbdbdf8e409d86d52fe43210f3dd731
hopper <hoppergoo@gmail.com>
2019-06-08 09:47:16 +0800

    feat: node 服务器

--- nodejs_入门/node/server.js
+++ nodejs_入门/node/server.js
@@ -2,16 +2,24 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    const file_name = './www' + req.url;
-
-    fs.readFile(file_name, (err, data) => {
+    const filePath = `www${req.url}`;
+    fs.readFile(filePath, (err, data) => {
         if (err) {
-            res.write('404');
+            fs.readFile('www/errors/NotFound.html', (err, data) => {
+                if (err) {
+                    res.writeHead(500);
+                    res.write('服务器错误！');
+                } else {
+                    res.writeHead(404);
+                    res.write(data);
+                }
+                res.end();
+            });
         } else {
             res.write(data);
+            res.end();
         }
-        res.end();
     });
 });
 
-server.listen(8080);
+server.listen(3000);
