3afe44a4a7ae379c9a3ec4b0529ba21d00c8177b
hopper <hoppergoo@gmail.com>
2019-06-20 08:06:42 +0800

    feat: websocket 通信

--- server.js
+++ server.js
@@ -34,14 +34,14 @@ const app = http.createServer((req, res) => {
                         res.write(JSON.stringify({code: 1, msg: '数据库错误'}));
                         res.end();
                     } else if (data.length > 0) {
-                        res.write(JSON.stringify({code:1, msg: '用户名已存在'}));
+                        res.write(JSON.stringify({code: 1, msg: '用户名已存在'}));
                         res.end();
                     } else {
                         pool.query(`INSERT INTO user_table (username, password, online) VALUES ('${username}', '${password}', 0)`, err => {
                             if (err) {
                                 res.write(JSON.stringify({code: 1, msg: '数据库错误'}));
                             } else {
-                                res.write(JSON.stringify({code:0, msg: '注册成功'}));
+                                res.write(JSON.stringify({code: 0, msg: '注册成功'}));
                             }
                             res.end();
                         })
@@ -98,33 +98,81 @@ app.listen(3000);
 
 // websocket
 const ws = io.listen(app);
+let sockArr = []; // 存通信的 sock ，用于广播
 ws.on('connection', sock => {
-   sock.on('reg', (username, password) => {
-       if (!regs.username.test(username)) {
-           sock.emit('reg_ret', 1, '用户名不符合规范');
-       } else if (!regs.password.test(password)) {
-           sock.emit('reg_ret', 1, '密码不符合规范');
-       } else {
-           pool.query(`SELECT ID FROM user_table WHERE username='${username}'`, (err, data) => {
-               if (err) {
-                   sock.emit('reg_ret', 1, '数据库错误');
-               } else if (data.length > 0) {
-                   sock.emit('reg_ret', 1, '用户名已存在');
-               } else {
-                   pool.query(`INSERT INTO user_table (username, password, online) VALUES ('${username}', '${password}', 0)`, err => {
-                       if (err) {
-                           sock.emit('reg_ret', 1, '数据库错误');
-                       } else {
-                           sock.emit('reg_ret', 0, '注册成功');
-                       }
-                   })
-               }
-           })
-       }
-   });
-   sock.on('login', (username, password) => {
-       // 登录
-   });
+    sockArr.push(sock);
+
+    let curUser = '';
+    let curUserId = '';
+
+    sock.on('reg', (username, password) => {
+        if (!regs.username.test(username)) {
+            sock.emit('reg_ret', 1, '用户名不符合规范');
+        } else if (!regs.password.test(password)) {
+            sock.emit('reg_ret', 1, '密码不符合规范');
+        } else {
+            pool.query(`SELECT ID FROM user_table WHERE username='${username}'`, (err, data) => {
+                if (err) {
+                    sock.emit('reg_ret', 1, '数据库错误');
+                } else if (data.length > 0) {
+                    sock.emit('reg_ret', 1, '用户名已存在');
+                } else {
+                    pool.query(`INSERT INTO user_table (username, password, online) VALUES ('${username}', '${password}', 0)`, err => {
+                        if (err) {
+                            sock.emit('reg_ret', 1, '数据库错误');
+                        } else {
+                            sock.emit('reg_ret', 0, '注册成功');
+                        }
+                    })
+                }
+            })
+        }
+    });
+    sock.on('login', (username, password) => {
+        if (!regs.username.test(username)) {
+            sock.emit('login_ret', 1, '用户名不符合规范');
+        } else if (!regs.password.test(password)) {
+            sock.emit('login_ret', 1, '密码不符合规范');
+        } else {
+            pool.query(`SELECT ID,password FROM user_table WHERE username='${username}'`, (err, data) => {
+                if (err) {
+                    sock.emit('login_ret', 1, '数据库错误');
+                } else if (data.length === 0) {
+                    sock.emit('login_ret', 1, '用户名不存在');
+                } else if (data[0].password !== password) {
+                    sock.emit('login_ret', 1, '用户名或密码不正确');
+                } else {
+                    pool.query(`UPDATE user_table SET online=1 WHERE ID=${data[0].ID}`, err => {
+                        if (err) {
+                            sock.emit('login_ret', 1, '数据库错误');
+                        } else {
+                            sock.emit('login_ret', 0, '登录成功');
+                            curUser = username;
+                            curUserId = data[0].ID;
+                        }
+                    })
+                }
+            });
+        }
+    });
+    sock.on('msg', txt => {
+        if (!txt) sock.emit('msg_ret', 1, '内容不能为空');
+        sockArr.forEach(item => { // 广播
+            if (item === sock) return;
+            item.emit('msg', curUser, txt);
+        });
+        sock.emit('msg_ret', 0, '发送成功', curUser, txt);
+    });
+    sock.on('disconnect', () => {
+        pool.query(`UPDATE user_table SET online=0 WHERE ID=${curUserId}`, err => {
+            if (err) {
+                console.log(`数据库错误：${err}`);
+            }
+            curUser = '';
+            curUserId = 0;
+            sockArr = sockArr.filter(item => item !== sock);
+        })
+    })
 });
 
 
@@ -139,4 +187,6 @@ ws.on('connection', sock => {
  * ws 接口
  * 客户端发送：'reg', username, password => 服务端返回： 'reg_ret', code, msg
  * 'login', username, password => 'login_ret', code, msg
+ * 'msg', txt => 'msg_ret', code, msg
+ *            => 'msg', username, msg
  */
