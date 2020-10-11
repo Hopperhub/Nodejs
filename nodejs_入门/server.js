e94bf3fab12ea372ec6a9855871a7904634104b7
戴赠记 <hopper@daizengjideMacBook-Pro.local>
2020-10-11 09:21:31 +0800

    [learn] 从 node.js 程序退出

--- nodejs_入门/server.js
+++ nodejs_入门/server.js
@@ -1,26 +1,31 @@
-const http = require('http'); // 载入 http 模块
-const fs = require('fs');
+const http = require('http');
+const url = require('url');
+// const fs = require('fs');
 
-fs.readFile('test.txt', (err, data) => {
-    console.log(err);
-    console.log(data); // Buffer（数据类型）: 缓冲区，二进制的数组，使数据保持二进制的状态，可以兼容文字、图片、视频... 
-})
-fs.writeFile('test1.txt', '写入的数据', err => {
-    console.log(err);
-})
+// fs.readFile('test.txt', (err, data) => {
+// 	console.log(err);
+// 	console.log(data); // Buffer（数据类型）: 缓冲区，二进制的数组，使数据保持二进制的状态，可以兼容文字、图片、视频... 
+// })
+// fs.writeFile('test1.txt', '写入的数据', err => {
+// 	console.log(err);
+// })
 
-http.createServer((request, response) => { // createServer 方法创建服务器; listen 方法绑定 8080 端口
+const server = http.createServer((req, response) => {
+	const query = url.parse(req.url).query;
+	if (query === 'a=1') {
+		process.kill(process.pid, 'SIGTERM');
+	}
 
-    // 发送 HTTP 头部
-    // 状态值 200：ok
-    // 内容类型：text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // 发送响应数据
-    response.write('123');
-    response.end();
-    // response.end('hello world'); // 响应
+	response.writeHead(200, { 'Content-Type': 'text/plain' });
+	response.write('123'); // 发送响应数据
+	response.end();
 }).listen(3000);
 
-// 终端 log
-console.log('Server running');
+process.on('SIGTERM', () => {
+	server.close(() => {
+		console.log('进程已终止');
+	});
+});
+
+console.log('服务器已就绪');
+console.log(process.pid);
